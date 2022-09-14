803804ad12f4118efe54476915a3f8b233896fda
Doppledouble <ian.suryadi@ui.ac.id>
2022-09-14 19:03:15 +0700

    update tugas 1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_catalog(request):
+        data_barang_Catalog = CatalogItem.objects.all()
+        context = {
+        'list_barang': data_barang_Catalog,
+        'nama': 'Ian Suryadi Timothy H ',
+        'npm' : '2106750875'
+        }
+        return render(request, "katalog.html",context)
