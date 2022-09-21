85bbf6ad3730807c1ba812620a335ee1c5f38cf6
Doppledouble <ian.suryadi@ui.ac.id>
2022-09-22 00:12:47 +0700

    tugas 3

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class BarangWatchlist(models.Model):
     watched= models.CharField(max_length=50)
-    title = models.IntegerField()
-    rating = models.TextField()
+    title = models.TextField()
+    rating = models.FloatField()
     release_date = models.TextField()
     review = models.TextField()
